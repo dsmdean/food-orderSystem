e15bcfeb0ae3282044ddfa7e3efaf926992b67c1
Dean Menso <deanmenso@gmail.com>
2016-05-19 23:11:53 -0400

    Just generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orderSystemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the orderSystemApp
+ */
+angular.module('orderSystemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
